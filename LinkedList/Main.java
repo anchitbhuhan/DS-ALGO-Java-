1a1ce714f1420f9a0ce76321492b389d2f12b01a
anchitbhuhan <bhushananchit@gmail.com>
2021-04-24 20:29:10 +0530

    changes

--- LinkedList/Main.java
+++ LinkedList/Main.java
@@ -87,52 +87,5 @@ class LinkedList
         }
     }
 
-    public void removenNthNode(int n)
-    {
-
-        // Node dummy = new Node(0);
-        // dummy.next = head;
-        // Node fast = dummy;
-        // Node slow = dummy;
-        // while(fast != null && n + 1 > 0) {
-        //     fast = fast.next;
-        //     n--;
-        // }
-        // while(fast != null) {
-        //     fast = fast.next;
-        //     slow = slow.next;
-        // }
-        // slow.next = slow.next.next;
-        // head = dummy.next;
-
-
-        int size= 0;
-        Node temp = head;
-        while(temp != null)
-        {
-            size++;
-            temp = temp.next;
-        }
-        temp = head;
-        
-        if(size==1 && n==1)
-            head = null;
-        
-        if(n>=size)
-            head = head.next;
-        else
-        {
-            int count = (size-n)-1;
-            int inc = 0;
-            while(inc != count && temp!= null)
-            {
-                inc++;
-                temp = temp.next;
-            }
-            temp.next = temp.next.next; 
-        }
-    }
-
-
 
 }
