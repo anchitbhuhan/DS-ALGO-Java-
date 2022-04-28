ddb5bee21c6d808fa36c9528aa23206fc47e9dce
Anchit Bhushan <bhushananchit@gmail.com>
2022-04-28 16:15:59 +0530

    Update OnetoN.cpp

--- Recursion/OnetoN.cpp
+++ Recursion/OnetoN.cpp
@@ -21,3 +21,31 @@ void print (int start, int n)
     cout << start << endl;
     print(start+1, n);
 }
+
+
+//=======================================================================//
+
+#include <iostream>
+
+using namespace std;
+void print(int n);
+
+int main ()
+{
+  int num;
+
+  cout << "Enter the value of N : " << endl;
+  cin >> num;
+
+  print (num);
+  return 0;
+}
+
+void print (int n)
+{
+    if (n <= 0)
+        return;
+    print(n-1);
+    cout << n << endl;
+    
+}
